c9221df70ed78676533e98beee0f0bc8b840e611
Mateo Lelong <mateo.lelong@datadoghq.com>
2026-03-20 13:16:47 +0100

    chore: remove podman support, sandbox id is not supported

--- comp/core/workloadmeta/collectors/internal/podman/podman.go
+++ comp/core/workloadmeta/collectors/internal/podman/podman.go
@@ -206,7 +206,7 @@ func convertToEvent(container *podman.Container) workloadmeta.CollectorEvent {
 			PID:        container.State.PID,
 			Ports:      ports,
 			Runtime:    workloadmeta.ContainerRuntimePodman,
-			SandboxID:  container.Config.Pod,
+			SandboxID:  "", // not supported https://github.com/containers/podman/issues/28291
 			State: workloadmeta.ContainerState{
 				Running:    container.State.State == podman.ContainerStateRunning,
 				Status:     status(container.State.State),
