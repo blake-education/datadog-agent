980d4aec26fcf7010610408fe8ab1c8369bf2792
louis-cqrl <louis.coquerelle@datadoghq.com>
2026-03-29 20:18:46 +0000

    fix(test): fix import cycle in submitter_test.go by using external test package

--- comp/process/submitter/impl/submitter_test.go
+++ comp/process/submitter/impl/submitter_test.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-package submitterimpl
+package submitterimpl_test
 
 import (
 	"testing"
